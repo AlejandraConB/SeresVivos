d64f851363af64d68d4889244306170948e9635d
Alejandra Contreras <user.email>
2024-12-13 20:13:11 -0300

    Primer commit

---
+++ src/Animales/Gato.java
@@ -0,0 +1,12 @@
+package Animales;
+
+public class Gato extends Animal {
+
+	  public void hacerRuido() {
+	        System.out.println("¡Miau!");
+	    }
+	
+	public void jugarConLaser() {
+        System.out.println("¡Jugando con el láser!");
+    }
+}
